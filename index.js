2efc59f9c3dac79bfff8f63dfcb2beedb23237ee
95u <72664813+im95u@users.noreply.github.com>
2021-06-24 06:42:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ var route = window.location.pathname;
 //console.log("✨ JsRoute [" + currentdate.getHours() + ":" + currentdate.getMinutes() + "] (Error x001) :\n")
 
 Jroute = (routeString, callbackfunction) => {
-    alert(route)
     // Route Setup  🔽
     if (routeString.startsWith(":")) {
-        alert("We Have URL parameter here")
+        alert("We Have URL parameter here v2.0")
     } else {
         if (route == routeString) {
             callbackfunction()
-        } else {
-            // Route is Wrong
-            console.warn("✨ JsRoute [" + currentdate.getHours() + ":" + currentdate.getMinutes() + "] :\n")
         }
     }
 };
